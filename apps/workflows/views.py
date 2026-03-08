30a8ca0d3b8e39ce378ad6f52f9f29b0e85f2b67
= <devblocks42@keemail.me>
2026-03-08 21:42:55 +0100

    Ajout du modèle Workflow; Formulaire d'envoi d'un document en vérification avec contrôle d'accès sur le document associé (Workflow)

--- apps/workflows/views.py
+++ apps/workflows/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import CreateWorkflowForm
+from django.contrib import messages
+from apps.documents.models import Document
+from django.http import Http404
+from django.utils import timezone
 
-# Create your views here.
+@login_required
+def create_workflow(request):
+    document_id = request.GET.get("id")
+    try:
+        document = get_object_or_404(Document, id=document_id)
+        if document.status != 'draft' or document.created_by != request.user:
+            messages.warning(request, "Le document que vous souhaitez soumettre n'est pas accessible.")
+            return redirect('list-documents')
+    except Http404:
+        messages.warning(request, "Le document que vous souhaitez soumettre n'existe pas.")
+        return redirect('list-documents')
+    if request.method == "GET":
+        form = CreateWorkflowForm()
+        return render(request, 'workflows/new_workflow.html', {
+            'form': form
+        })
+    elif request.method == "POST":
+        form = CreateWorkflowForm(request.POST)
+        if form.is_valid():
+            workflow = form.save(commit=False)
+            workflow.document = document
+            workflow.document.status = 'pending'
+            workflow.performed_by = request.user
+            workflow.performed_at = timezone.now()
+            workflow.save()
+            workflow.document.save()
+            messages.success(request, "Le document a bien été envoyé en validation.")
+    return redirect('list-documents')
